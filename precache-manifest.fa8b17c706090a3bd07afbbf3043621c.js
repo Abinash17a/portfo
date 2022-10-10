17d7bf7f04172589e81b21d4a6f728384de4f57a
Abinash Chhetri <77913801+Abinash17a@users.noreply.github.com>
2022-10-10 22:44:19 +0545

    Add files via upload

---
+++ precache-manifest.fa8b17c706090a3bd07afbbf3043621c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bae15d69f9cb0a65bc7f",
+    "url": "/Abinash17a/portfo.git/static/css/main.0ebe6d46.chunk.css"
+  },
+  {
+    "revision": "bae15d69f9cb0a65bc7f",
+    "url": "/Abinash17a/portfo.git/static/js/main.bae15d69.chunk.js"
+  },
+  {
+    "revision": "4cc81509af91a72db11b",
+    "url": "/Abinash17a/portfo.git/static/js/1.4cc81509.chunk.js"
+  },
+  {
+    "revision": "5668bca80787e15c16b8",
+    "url": "/Abinash17a/portfo.git/static/js/runtime~main.5668bca8.js"
+  },
+  {
+    "revision": "6f820574e0c1d3087097684bba9e1def",
+    "url": "/Abinash17a/portfo.git/static/media/webdev.6f820574.jpg"
+  },
+  {
+    "revision": "887afe1b320569750a2eac301292f8c3",
+    "url": "/Abinash17a/portfo.git/static/media/webdevc.887afe1b.jpg"
+  },
+  {
+    "revision": "31fa29769a856770d5f927106b6b5026",
+    "url": "/Abinash17a/portfo.git/static/media/webdeva.31fa2976.jpg"
+  },
+  {
+    "revision": "d0c520fb4554749e57859e8eb914b4c0",
+    "url": "/Abinash17a/portfo.git/static/media/newabout.d0c520fb.jpg"
+  },
+  {
+    "revision": "f1b40ff9617a4dcf9bfc115ef6f274b6",
+    "url": "/Abinash17a/portfo.git/static/media/webdev1.f1b40ff9.jpg"
+  },
+  {
+    "revision": "f7584b2a1e08f5bdd66555640f037dd0",
+    "url": "/Abinash17a/portfo.git/static/media/TExter.f7584b2a.png"
+  },
+  {
+    "revision": "fdc10367acdaacd7947c5a27bdb7423c",
+    "url": "/Abinash17a/portfo.git/static/media/drum.fdc10367.png"
+  },
+  {
+    "revision": "5f1004df7ddc4913687e7f6f1b0af550",
+    "url": "/Abinash17a/portfo.git/index.html"
+  }
+];
